d6b35d40f3ae58f1beafa0e313482ec258857e9c
viniciusz1 <vinicius101313@gmail.com>
2022-06-13 14:35:52 -0300

    rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { LoginModule } from './login/login.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent,
+        canActivate: []
+      }
+    ]),
+    FormsModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
