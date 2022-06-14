a4e22c43dd9885d12e65b5c75c89b9b90152bf76
viniciusz1 <vinicius101313@gmail.com>
2022-06-13 21:46:58 -0300

    poi

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component: LoginComponent,
+        canActivate: []
+      }
+    ]),
   ]
 })
 export class LoginModule { }
