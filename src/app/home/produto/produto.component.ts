641c3e10834aba3450615b661951eb80a268690d
Vinícius Bonatti Benner <vinicius101313@gmail.com>
2022-07-09 19:41:48 -0300

    pedidos

--- src/app/home/produto/produto.component.ts
+++ src/app/home/produto/produto.component.ts
@@ -1,3 +1,4 @@
+import { CarrinhoService } from 'src/app/services/carrinho.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Produto } from 'src/app/shared/produto.model';
@@ -8,14 +9,15 @@ import { Produto } from 'src/app/shared/produto.model';
 })
 export class ProdutoComponent implements OnInit {
   @Input() produto: Produto = new Produto("1", "2","3","4", "5", "6", 7);
-  adicionarCarrinho(){
-    console.log("adicionarCarrinho")
-  }
+ 
   abreDetalhesProduto(){
     this.router.navigate(['/detalhes-produto'])
   }
-  constructor(private router: Router) { }
-
+  constructor(private router: Router,
+    private carrinhoService: CarrinhoService) { }
+    adicionarCarrinho(){
+      this.carrinhoService.addProduto(this.produto)
+    }
   ngOnInit(): void {
   }
 
