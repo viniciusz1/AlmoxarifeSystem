8d155ec09675b032effab2399e2bbd1000cb0a05
viniciusz1 <vinicius101313@gmail.com>
2022-07-11 12:01:03 -0300

    meio filtro ta indo

--- src/app/pipes/filtro-especializado-produtos.pipe.ts
+++ src/app/pipes/filtro-especializado-produtos.pipe.ts
@@ -1,7 +1,7 @@
 import { Produto } from 'src/app/shared/produto.model';
 import { Pipe, PipeTransform } from '@angular/core';
 
-
+//Código e nome do produto estão funcionais
 
 @Pipe({
   name: 'filtroEspecializadoProdutos'
@@ -9,25 +9,27 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FiltroEspecializadoProdutosPipe implements PipeTransform {
 
   transform(listaProdutos: Produto[], filtragem: Produto): Produto[] {
-    
+    const codigo = listaProdutos.map(e => e.codigo == filtragem.codigo)
     const classificacao = listaProdutos.map(e => e.classificacao.startsWith(filtragem.classificacao))
-    // console.log(filtragem.classificacao, listaProdutos[0].classificacao)
-    let codigo: boolean[] = []
-    let desbug = 0
-    if(!(filtragem.codigo == 1 && desbug == 0)){
-      
-      console.log("filtragem.codigo")
-    }
     const descricao = listaProdutos.map(e => e.descricao.startsWith(filtragem.descricao))
     const localidade = listaProdutos.map(e => e.localidade.startsWith(filtragem.localidade))
     const nome = listaProdutos.map(e => e.nome.startsWith(filtragem.nome))
     const opcaoUso = listaProdutos.map(e => e.opcaoUso.startsWith(filtragem.opcaoUso))
-    // console.log(filtragem.opcaoUso, listaProdutos[0].opcaoUso)
     const quantidade = listaProdutos.map(e => e.quantidade.startsWith(filtragem.quantidade))
     const novaLista = []
-    console.log("classificacao:", classificacao,"codigo", codigo,"descricao", 
-    descricao, "localidade",localidade, "nome",nome, "opcaoUso",opcaoUso,"quantidade", quantidade)
+
+    // console.log(filtragem.codigo)
+    if(filtragem.codigo == -1){
+      return listaProdutos
+    }
+    
     for(let i = 0; i < listaProdutos.length; i++){
+      
+      if(!filtragem.codigo){
+        codigo[i] = true
+      }
+      // console.log("classificacao:", classificacao,"codigo", codigo,"descricao", 
+      // descricao, "localidade",localidade, "nome",nome, "opcaoUso",opcaoUso,"quantidade", quantidade)
       if(classificacao[i] && codigo[i] && descricao[i] && localidade[i] && nome[i] && opcaoUso[i] && quantidade[i]){
         novaLista.push(listaProdutos[i])
       }
