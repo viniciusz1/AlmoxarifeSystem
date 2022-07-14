4be58e35e35b0791a1ed21ebc6157604911b83bb
viniciusz1 <vinicius101313@gmail.com>
2022-07-14 19:51:25 -0300

    a

--- src/app/contas/perfil-admin/perfil-admin.component.ts
+++ src/app/contas/perfil-admin/perfil-admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-perfil-admin',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilAdminComponent implements OnInit {
 
+  @Output() estadoPerfil = new EventEmitter<boolean>();
   constructor() { }
 
   ngOnInit(): void {
