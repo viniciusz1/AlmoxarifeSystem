efe3c7574b32bb452915551be18d8179cd9e3745
viniciusz1 <vinicius101313@gmail.com>
2022-07-16 14:13:24 -0300

    a

--- src/app/checklogged.ts
+++ src/app/checklogged.ts
@@ -5,7 +5,7 @@ import {
     Router,
     RouterStateSnapshot
 } from "@angular/router";
-import { Observable } from "rxjs";
+import { Observable } from "rxjs"
 
 @Injectable()
 class CheckLogged implements CanActivate {
