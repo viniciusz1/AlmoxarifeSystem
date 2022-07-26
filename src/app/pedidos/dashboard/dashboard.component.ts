9cec8f8104d581b31c61002c1545b8bfce2a40ff
viniciusz1 <vinicius101313@gmail.com>
2022-07-26 11:53:53 -0300

    arrumando estilos dashboard

--- src/app/pedidos/dashboard/dashboard.component.ts
+++ src/app/pedidos/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PedidosService } from 'src/app/services/pedidos.service';
+import { Pedido } from 'src/app/shared/pedido.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,7 +11,9 @@ export class DashboardComponent implements OnInit {
 
   constructor(private pedidosService: PedidosService) { }
   selected = new Date()
+  entregas: Pedido[] = []
   ngOnInit(): void {
+    this.entregas = this.pedidosService.getEntregas()
   }
 
 }
